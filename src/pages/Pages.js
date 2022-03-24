64f28beb4ac4254b7daddb1423650fb891a3c778
Fernando Vega <fjbvega.22@gmail.com>
2022-03-24 09:24:24 -0300

    Commit Build update6

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
 import { Navbar } from '../components/Navbar'
 import { DetailsRecipes } from './DetailsRecipes'
 import { DairyFree } from './DairyFree'
@@ -10,7 +10,7 @@ import { ErrorView } from '../components/ErrorView'
 
 export const Pages = () => {
   return (
-      <HashRouter> 
+      <HashRouter basename="/"> 
         <Navbar />       
         <Routes>            
             <Route path='/' exact element={<Home />} />
